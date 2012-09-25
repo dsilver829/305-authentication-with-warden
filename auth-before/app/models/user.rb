a1fda7ff8127513b208d1db6ae71b10abd982877
David Silver <dsilver@candidatemetrics.com>
2012-09-25 09:36:38 -0400

    setting up

--- auth-before/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
-  validates_presence_of :password, on: :create
-  validates_presence_of :email
-  has_secure_password
-end
--- auth/db/schema.rb
+++ auth/db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20111203202102) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
